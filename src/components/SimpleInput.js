8117883b2f4b39b2606b9a530305398cc76dc83a
Gray Loki <skuc090@gmail.com>
2022-07-19 16:45:15 +0300

    add validation for form

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,14 @@
-import {useEffect, useState} from "react";
+import { useState} from "react";
 import '../index.css';
 const SimpleInput = (props) => {
   const [enteredName,setEnteredName]=useState("");
   const [enteredNameTouched,setEnteredNameTouched]=useState(false);
   const enteredNameIsValid=enteredName.trim()!=='';
   const nameInputIsInvalid=!enteredNameIsValid && enteredNameTouched;
-
+  let formIsValid=false;
+    if(enteredNameIsValid){
+        formIsValid=true;
+    }
     const nameInputChangeHandler=event=>{
       setEnteredName(event.target.value);
 
@@ -40,7 +43,7 @@ const SimpleInput = (props) => {
           {nameInputIsInvalid && <p className="error-text">Name must not be empty!</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
