06353c0e6385819b1cda7426950fbfb631a286b1
Gray Loki <skuc090@gmail.com>
2022-07-19 13:21:21 +0300

    add two ways for getting value from input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,23 @@
+import {useRef, useState} from "react";
+
 const SimpleInput = (props) => {
-  return (
-    <form>
+    const nameInputRef=useRef();
+  const [enteredName,setEnteredName]=useState("");
+  const nameInputChangeHandler=event=>{
+      setEnteredName(event.target.value);
+  }
+  const forSubmissionHandler=event=>{
+      event.preventDefault();
+      console.log(enteredName);
+      const enteredValue=nameInputRef.current.value;
+      console.log(enteredValue);
+      setEnteredName('');
+  }
+    return (
+    <form onSubmit={forSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
